c00fb35f609632c1104159ec40901c337f5839b4
shohamsanyal <shohamsanyal@gmail.com>
2022-11-13 10:20:41 -0600

    Added graph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ import Definition from './components/Definition';
 import GridContainer from './components/GridContainer';
 import {Item, SubItem} from './components/MenuItem'
 
+import APPL from'./json/APPL.json'
+import AMZN from './json/AMZN.json'
+import FXAIX from './json/FXAIX.json'
+import GOOGL from './json/GOOGL.json'
+import INTC from './json/INTC.json'
+import JEPI from './json/JEPI.json'
+import JNJ from './json/JNJ.json'
+import JPST from './json/JPST.json'
+import MA from './json/MA.json'
+import META from './json/META.json'
+import MSFT from './json/MSFT.json'
+import NVDA from './json/NVDA.json'
+import QQQ from './json/QQQ.json'
+import SPY from './json/SPY.json'
+import TSLA from './json/TSLA.json'
+import V from './json/V.json'
+import VOO from './json/VOO.json'
+import VFAIX from './json/VFIAX.json'
+import VSMPX from './json/VSMPX.json'
+
+import BTC from './json/BTC-USD.json'
+import DOGE from './json/DOGE-USD.json'
+import ETH from './json/ETH-USD.json'
+import SOL from './json/SOL-USD.json'
+import XRP from './json/XRP-USD.json'
+import Graph from './components/Graph';
+
 function App() {
   return (
     <div>
---
+++ src/components/Graph.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { useState } from "react";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+
+function convertEpochToSpecificTimezone(timeEpoch, offset){
+  var d = new Date(timeEpoch);
+  var utc = d.getTime() + (d.getTimezoneOffset() * 60000);  //This converts to UTC 00:00
+  var nd = new Date(utc + (3600000*offset));
+  return nd.toLocaleString();
+}
+
+function dataGenerator(company) {
+    let companyData = []
+    
+    //key for each price
+    for (let i = 1556582400000; i <= 1668124800000; i = i + 86400000) {
+      let dailyCompanyData = {
+        
+        date: convertEpochToSpecificTimezone(i, 0),
+        unix: i,
+        Close: company.Close[i],
+        SMA: company.SMA_100[i],
+        EMA: company.EMA_100[i],
+        UpperBand: company.Upperband[i],
+        LowerBand: company.Lowerband[i]
+    
+      };
+      companyData.push(dailyCompanyData);
+    }
+
+    return companyData
+}
+
+export default function Graph(props) {
+    let dates = dataGenerator(props.companyFile)
+
+    console.log(props.companyFile)
+    
+    return (
+    <div className='App'>
+      <div className='Brother-man'>
+
+      </div>
+      <header>
+        <div>
+          <LineChart
+            width={1500}
+            height={750}
+            // This is the variable to be changed
+            data={dates}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey = 'date' />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              connectNulls
+              type='line'
+              dataKey="Close"
+              stroke="#ff0000"
+              dot={false}
+            />
+
+            {/* SMA_100 */}
+            {/* <Line
+              connectNulls
+              type="monotone"
+              dataKey="SMA"
+              stroke="#0000ff"
+              dot={false}
+            /> */}
+            {/* EMA_100 */}
+            <Line
+              connectNulls
+              type="monotone"
+              dataKey="EMA"
+              stroke="#00ff00"
+              dot={false}
+            />
+
+            {/* BBANDS */}
+
+            {/* <Line
+              connectNulls
+              type="line"
+              dataKey="UpperBand"
+              stroke="#918e8e"
+              dot={false}
+            />
+            <Line
+              connectNulls
+              type="line"
+              dataKey="LowerBand"
+              stroke="#918e8e"
+              dot={false}
+            /> */}
+
+          </LineChart>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
