7717855a714f6689d6a319c1e2c78bf4c9e3dc99
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-11-13 01:23:37 -0600

    formatted menu bar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import Definition from './components/Definition';
 import GridContainer from './components/GridContainer';
 import {Item, SubItem} from './components/MenuItem'
 
+import LearnIcon from './assets/icons/Learn.svg'
+import StocksIcon from './assets/icons/Stocks.svg'
+import CryptoIcon from './assets/icons/Crypto.svg'
+import SuggestionsIcon from './assets/icons/Suggestions.svg'
+
+
 function App() {
   return (
-    <div>
-      <div>
-        <Item icon="./" label="Education" />
+    <div className='app-div'>
+      <div className='menu-div'>
+        <Item icon={LearnIcon} label="Education" />
         <SubItem label="Technical" />
         <SubItem label="Fundamental" />
+        <Item icon={StocksIcon} label="Stocks" />
+        <Item icon={CryptoIcon} label="Crypto" />
+        <Item icon={SuggestionsIcon} label="Get Suggestions" />
       </div>
       <GridContainer>
         <Definition term="Ur Mom" definition="Your mom is very very large its hard to even fit her in this card lmfao" />
