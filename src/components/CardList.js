3857334d4462a05b373828418cb428463b19c48f
shohamsanyal <shohamsanyal@gmail.com>
2022-11-13 11:40:57 -0600

    fixed search in stocks

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -11,8 +11,8 @@ export default function CardList({children}) {
             <input onChange={(event) => {setSearchTerm(event.target.value)}} className="search" type="text" placeholder="Search..."></input>
             
             <div className="content-container">
-                {children}
-                {/* {children.filter((card) => { card.props.ticker.toLowerCase().includes(searchTerm.toLowerCase()) || card.props.name.toLowerCase().includes(searchTerm.toLowerCase())})} */}
+                {/* {children} */}
+                {children.filter((card) => {return card.props.ticker.toLowerCase().includes(searchTerm.toLowerCase()) || card.props.name.toLowerCase().includes(searchTerm.toLowerCase())})}
             </div>
         </div>
     )
